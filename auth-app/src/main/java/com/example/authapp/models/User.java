5b4074d7b8daaa147cb64209ea58abd4fc56d222
Sithija Shehara <nsithijashehara@gmail.com>
2022-09-11 23:49:21 +0530

    added models

--- auth-app/src/main/java/com/example/authapp/models/User.java
+++ auth-app/src/main/java/com/example/authapp/models/User.java
@@ -1,4 +1,23 @@
 package com.example.authapp.models;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(	name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        })
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    public User() {
+    }
 }
