e9b3afafae99de81dbd4459f4af9a260b22cae32
Sithija Shehara <nsithijashehara@gmail.com>
2022-11-08 22:33:07 +0530

    code change - repository

--- auth-app/src/main/java/com/example/authapp/repository/RoleRepository.java
+++ auth-app/src/main/java/com/example/authapp/repository/RoleRepository.java
@@ -1,4 +1,13 @@
 package com.example.authapp.repository;
 
-public interface RoleRepository {
+import com.example.authapp.models.ERole;
+import com.example.authapp.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
 }
--- auth-app/src/main/java/com/example/authapp/repository/UserRepository.java
+++ auth-app/src/main/java/com/example/authapp/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package com.example.authapp.repository;
 
-public interface UserRepository {
+import com.example.authapp.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
