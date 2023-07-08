7dab149d62c6cfd9b519136c94d74abe4ae06e87
Sithija Shehara <nsithijashehara@gmail.com>
2023-07-08 20:01:31 +0530

    completed - crud api

--- traversy media MERN course/api/server.js
+++ traversy media MERN course/api/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const colors = require("colors");
 const port = process.env.PORT || 5000;
 const router = require("./routes/goalRoutes");
+const connectDB = require("./config/db");
 const errorHandler = require("./middleware/errorMiddleware");
 
 const app = express();
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
