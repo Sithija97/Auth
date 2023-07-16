bcf7135c188841c29b592c32d5640ad6744cfe51
Sithija Shehara <nsithijashehara@gmail.com>
2023-07-16 13:32:36 +0530

    auth api implementation

--- traversy media MERN course - 2/api/server.js
+++ traversy media MERN course - 2/api/server.js
@@ -1,11 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import colors from "colors";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.get("/", (req, res) => res.send("Server is ready"));
-
-app.listen(PORT, () => console.log(`Server started on port : ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port : ${PORT}`.magenta));
